f68f7120f05c3bf3035dd7ae6d4d54ee40fdb4ea
Ian Hofmann-Hicks (evil) <evilsoft@aol.com>
2016-05-07 15:45:12 -0700

    move to a use flow

--- src/lib/render/actionDispatch.js
+++ src/lib/render/actionDispatch.js
@@ -1,3 +1,8 @@
-export default function actionDispatch(action) {
-  return (dispatch, data) => () => dispatch(action(data))
-}
+import curry    from 'ramda/src/curry'
+import compose  from 'ramda/src/compose'
+
+const actionDispatch = curry(
+  (action, dispatch, data) => () => compose(dispatch, action)(data)
+)
+
+export default actionDispatch
--- src/render/js/components/UuidItem.js
+++ src/render/js/components/UuidItem.js
@@ -3,42 +3,38 @@ import { clipboard } from 'electron'
 
 import compose from 'ramda/src/compose'
 
-import concatIf       from 'shared/concatIf'
 import actionDispatch from 'render/actionDispatch'
 
 import { deleteUuid } from 'actions/uuid'
 
-const texttoClip = clip => uuid => () => clip.writeText(uuid)
-
+const texttoClip  = clip => uuid => () => clip.writeText(uuid)
 const copyItem    = texttoClip(clipboard)
+
 const deleteItem  = actionDispatch(deleteUuid)
 
 function controller(attrs) {
-  const { uuid, dispatch, index } = attrs
-  const remove = deleteItem(dispatch, index)
+  const { dispatch } = attrs
+  const remove = deleteItem(dispatch)
 
   return { copyItem, remove }
 }
 
 function view(ctrl, attrs) {
-  const { uuid } = attrs
-  const { copyItem, remove } = ctrl
+  const { index } = attrs
+  const { uuid }  = attrs.uuid
 
-  const used    = concatIf('uuid__item--used', uuid.used)
-  const classes = used([ 'uuid__item' ]).join(' ')
+  const { remove, copyItem } = ctrl
+
+  const useUuid = compose(remove(index), copyItem(uuid))
 
   return (
-    <li className={classes}>
-      <span className="uuid__uuid">{uuid.uuid}</span>
+    <li className="uuid__item">
+      <span className="uuid__uuid">{uuid}</span>
       <div className="uuid__buttons form--inline">
         <button
           className="button"
-          onclick={copyItem(uuid.uuid)}
-          type="button">Copy</button>
-        <button
-          className="button"
-          onclick={remove}
-          type="button">Delete</button>
+          onclick={useUuid}
+          type="button">Use</button>
       </div>
     </li>
   )
